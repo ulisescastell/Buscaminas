5371fa3fbf38b6b76f17f800f86f36cfd0368e0c
ulisescastell <ulises.castell.7e7@itb.cat>
2024-03-22 13:44:20 +0100

    add more styles to casilla

--- controllers/main.js
+++ controllers/main.js
@@ -84,7 +84,7 @@ function revelarCasilla(x, y, casillaDiv) {
     } else {
         let adyacentes = tablero.tablero[x][y].adyacentes;
         if (adyacentes > 0) {
-            casillaDiv.classList.add(['uno', 'dos', 'tres', 'cuatro', 'cinco'][adyacentes-1]);
+            casillaDiv.classList.add(['uno', 'dos', 'tres', 'cuatro', 'cinco', 'seis', 'siete', 'ocho'][adyacentes-1]);
         }
     }
 }
@@ -158,41 +158,6 @@ function crearBoton(mensaje) {
     content.appendChild(btn)
 }
 
-
-function fire(particleRatio, opts) {
-    confetti(
-      Object.assign({}, defaults, opts, {
-        particleCount: Math.floor(count * particleRatio),
-      })
-    );
-  }
-  
-  fire(0.25, {
-    spread: 26,
-    startVelocity: 55,
-  });
-  
-  fire(0.2, {
-    spread: 60,
-  });
-  
-  fire(0.35, {
-    spread: 100,
-    decay: 0.91,
-    scalar: 0.8,
-  });
-  
-  fire(0.1, {
-    spread: 120,
-    startVelocity: 25,
-    decay: 0.92,
-    scalar: 1.2,
-  });
-  
-  fire(0.1, {
-    spread: 120,
-    startVelocity: 45,
-  });
 
 
 
